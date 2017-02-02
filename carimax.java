bdaba04e7be875f982778d99e97504801f96e48d
HADES <alvianabdiana07@gmail.com>
2017-02-02 18:54:01 +0700

    Mencari Nilai Max

---
+++ carimax.java
@@ -0,0 +1,15 @@
+class carimax{
+public static void main(String [] arg)
+{
+int angka1 = 70 ;
+int angka2 = 20 ;
+int angka2 = 95 ;
+ 
+int max = 0 ;
+max = angka1 >= max ? angka1 : max;
+max = angka2 >= max ? angka2 : max;
+max = angka3 >= max ? angka3 : max;
+
+System.out.println ("Angka Yang Paling Besar = "+max);
+}
+}
